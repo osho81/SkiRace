165dc8004ca556b24e4808951433dc96188f0fae
JonathanAstrom <astrom.jonathan@gmail.com>
2021-12-07 10:29:24 +0100

    Add SkierReg

--- src/com/ya/grupp5b/skirace/application/Main.java
+++ src/com/ya/grupp5b/skirace/application/Main.java
@@ -1,10 +1,12 @@
 package com.ya.grupp5b.skirace.application;
 
+import com.ya.grupp5b.skirace.skier.SkierReg;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		SkierReg.addSkier(); //bara för att testa
 
 	}
 
