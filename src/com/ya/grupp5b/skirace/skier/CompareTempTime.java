b92014d384eed50417d3f799f124061e42f64981
JonathanAstrom <astrom.jonathan@gmail.com>
2022-01-04 15:58:04 +0100

    åtgärdat pernillas påpekning, bra där!

--- src/com/ya/grupp5b/skirace/skier/CompareTempTime.java
+++ src/com/ya/grupp5b/skirace/skier/CompareTempTime.java
@@ -6,22 +6,7 @@ import java.util.Comparator;
 public class CompareTempTime implements Comparator<Skier> {
 
 	public int compare(Skier skier1, Skier skier2) {
-//		LocalTime tempTime1 = skier1.getTempTime();
-//		LocalTime tempTime2 = skier2.getTempTime();
-//		// Kan lägga till nowTime.plus(15 * skier1.getStartNr)
-//		Duration duration1 = Duration.between(tempTime1, skier1.getIndivStartTime());
-//		int durationInSec1 = (int) duration1.toSeconds();
-//		Duration duration2 = Duration.between(tempTime2, skier2.getIndivStartTime());
-//		int durationInSec2 = (int) duration2.toSeconds();
-//
-//		if (durationInSec1 < durationInSec2) {
-//			return -1;
-//		} else if (durationInSec1 > durationInSec2) {
-//			return 1;
-//		} else {
-//			return 0;
-//		}
-		////////////////////////////////////////////////
+
 		if (skier1.getTempTime() == null) {
 			return -1;
 		} else if (skier2.getTempTime() == null) {
